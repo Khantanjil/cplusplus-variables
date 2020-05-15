3df1a4e0eb20062120de4df667660d189b5b9f83
Tanjil <tanjilkhan.sh@gmail.com>
2020-05-15 13:12:11 +0100

    Add files via upload

---
+++ variables/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int myNum = 10; // Integer
+    double myDouble = 4.532134; // Float with high capacity
+    float myFloat = 10.5; // Float with low capacity
+    char myChar = 'D'; // One character
+
+    string myString = "An string"; // String text
+    cout << myNum << endl; // Print out the variable myNum (integer)
+    cout << myDouble << endl; // Print out the variable myDouble (float)
+    cout << myFloat << endl; // Print out the variable myFLoat (float)
+    cout << myChar << endl; // Print out the variable myChar (Letter)
+    cout << myString << endl; // Print out the variable myChar (string text)
+
+    return 0;
+}
